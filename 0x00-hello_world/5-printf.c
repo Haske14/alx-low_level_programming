8f970c38e2c912daf28344f5efb81adf7e3f8251
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-14 06:12:01 -0700

    Redo of printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - print the spring in the put function
+* main - print the spring in the printf function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* this program prints "with proper grammar, but the outcome is a piece of art,"
 * Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return: (0)
+	return: (0);
 }
