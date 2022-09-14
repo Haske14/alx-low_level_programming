740d34207200ecf979f47923045fbddf60ae5d77
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-14 04:10:55 -0700

    Using the puts funtions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print the spring in the puts function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+init main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
