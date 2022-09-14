c8c30d0780323abfe7c67cda73b609d763ec0e01
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-14 04:15:16 -0700

    Redo for puts funtions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#!/bin/bash 
 #include <stdio.h>
 /**
 * main - print the spring in the puts function
@@ -5,7 +6,7 @@
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle"
 * Return: 0
-*/
+**/
 init main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
