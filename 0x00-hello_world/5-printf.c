04c02b4c33c64adfa613ed982a00a75e3ebc0037
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-14 05:01:58 -0700

    function for printtf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the spring in the printf function
+*
+* Description: using the main function
+* this program prints "with proper grammer, but the outcome is a piece of art,
+* Return: 0
+*/
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art,");
+	return: (0);
+}
