2fbbfa25f3e52fa4d580f77f8843d3750c927bb7
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-22 15:01:32 -0700

    Hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,24 +7,16 @@ void print_alphabet(void)
 {
 		char ch;
 
-
-
 			ch = 'a';
-
 
 
 				while (ch <= 'z')
 
 						{
-
 									_putchar(ch);
-
 											ch++;
 
 												}
 
-
-
 					_putchar('\n');
-
 }
