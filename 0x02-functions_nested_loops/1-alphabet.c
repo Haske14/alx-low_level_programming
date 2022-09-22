a1e978103c2b1b35adc38c5f76e6afd68b3e461d
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-22 08:33:10 -0700

    Hot fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
  * main - prints alphabet in lowecase and new line
  */
+
 void print_alphabet(void)
 {
 	int j;
+
 	for (j = 'a'; j <= 'z'; j++)
+	{
 		putchar(j);
+	}
 	_putchar('\n');
 }
