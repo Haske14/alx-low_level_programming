15b909533cf87e9d54cc51e97a9e5c2778690c2b
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-23 08:08:43 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,12 @@
  * print_numbers -> print numbers
  *
  */
+
 void print_numbers(void)
 {
-	int x;
+		int x;
 
-	for (x = 0; x < 10; x++)
-		_putchar(x + '0');
-	_putchar('\n');			
+		for (x = 0; x < 10; x++)
+			_putchar(x + '0');
+		_putchar('\n');
 }
