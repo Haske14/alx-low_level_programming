6788e356f75cad7860e3131496942ce228f9c7bc
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-22 17:27:58 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha -> function that checks for alphabet character
+ * @c: character argument
+ * Return: return 1 or 0
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c 
+<= 'Z'));
+}
