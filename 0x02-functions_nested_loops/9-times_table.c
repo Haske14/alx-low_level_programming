63e61fd9797c64cdc0063f57dcc0052b1ff0e990
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-22 22:38:19 -0700

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,15 +13,15 @@ void times_table(void)
 			for (y = 0; y < 10; y++)							{
 				k = x * y;
 				if (y == 0)
-					_putchar(k + '0');
+					   _putchar(k + '0');
 				if (y != 0 && k < 10)
-				{
+				{	
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
-				} else if (k >= 10)
-
+				}
+				else if (k >= 10)
 				{
 					_putchar(',');
 					_putchar(' ');
