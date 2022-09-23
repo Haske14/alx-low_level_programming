22eb487099bd7c448ad67270a2aa39f02ff69db0
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-23 02:00:42 -0700

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,13 +2,10 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints numbers to 98
+ * main - prints numbers to 98
  * @n: starting integer
- *
  * Description: prints all natural number from n - 98
- *
  * Return: void
- *
  */
 void print_to_98(int n)
 {
