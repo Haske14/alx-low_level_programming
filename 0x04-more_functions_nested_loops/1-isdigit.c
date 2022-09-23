e0b19919a4c17ef86fd6271c4cb33f487981a605
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-23 06:32:49 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,27 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
+#include "main.h"
 
 /**
  *
- * main - prints the last digit of the random
+ * _isdigit - funtion to check for a digit
  *
- * number stored in the variable n
+ * @c: an integer argument
  *
- * Return: Always 0 (Success)
- *
+ * Return: success
  */
-int main(void)
+int _isdigit(int c)
 {
-		int n;
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		printf("Last digit of %d is %d ", n, n % 10);
-		if (n % 10 > 5)
-			printf("and is greater than 5\n");
-		else if (n % 10 == 0)								printf("and is 0\n");
-		else if (n % 10 < 6 && n % 10 != 0)
-			printf("and is less than 6 and not 0\n");
-		return (0);
+
+		return (c >= '0' && c <= '9');
+
 }
