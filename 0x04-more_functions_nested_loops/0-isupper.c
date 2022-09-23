fbe8193884ff825df35e078690f0b3e85e4e1bb2
Festus Abiodun <abiodunfestus500@gmail.com>
2022-09-23 05:33:23 -0700

    functions that check uppercase in c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,19 +3,17 @@
 
 
 /**
-
-* _isupper -> this is a function to check upper case alphas
-
-* @c: an integer paramer
-
-* Return: something but success
-
-*/
-
+ * _isupper -> this is a function to check upper case alphas
+ *
+ * @c: an integer paramer
+ *
+ * Return: something but success
+ *
+ */
 int _isupper(int c)
 
 {
 
-	return (c >= 'A' && c <= 'Z');
+		return (c >= 'A' && c <= 'Z');
 
 }
